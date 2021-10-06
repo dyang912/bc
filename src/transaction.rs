dc3484ef34d929676b5b9b83f289c2f4c11b8086
Dong Yang <outskidding@gmail.com>
2021-10-05 23:41:55 -0500

    warmup2

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,18 +1,26 @@
 use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+use ring::signature::{Ed25519KeyPair, Signature, KeyPair};//, VerificationAlgorithm, EdDSAParameters};
+use ring::digest;
+use ring::rand::SecureRandom;
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Transaction {
+    msg: String
 }
 
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    let serialized = bincode::serialize(t).unwrap();
+    let msg = digest::digest(&digest::SHA256, &serialized);
+    key.sign(msg.as_ref())
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &Transaction, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey, signature: &Signature) -> bool {
-    unimplemented!()
+    let serialized = bincode::serialize(t).unwrap();
+    let msg = digest::digest(&digest::SHA256, &serialized);
+    let peer_public_key = ring::signature::UnparsedPublicKey::new(&ring::signature::ED25519, public_key.as_ref());
+    peer_public_key.verify(msg.as_ref(), signature.as_ref()).is_ok()
 }
 
 #[cfg(any(test, test_utilities))]
@@ -21,8 +29,10 @@ mod tests {
     use crate::crypto::key_pair;
 
     pub fn generate_random_transaction() -> Transaction {
-        Default::default()
-        //unimplemented!()
+        let sr = ring::rand::SystemRandom::new();
+        let mut result = [0u8; 32];
+        sr.fill(&mut result).unwrap();
+        Transaction{msg: result[0].to_string()}
     }
 
     #[test]
